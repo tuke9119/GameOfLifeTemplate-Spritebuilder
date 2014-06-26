b69943c0fc294a511f4f580fad47bed113693552
Tushig Ochirhkuyag <Tuke@Tushigs-MacBook-Pro.local>
2014-06-26 16:54:54 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -83,6 +83,7 @@ static const int GRID_COLUMNS = 10;
 {
     [self countNeighbors]; //update each creature's neighbor count
     [self updateCreatures];//update each creature's state
+    self.generation +=1;
 }
 -(void)countNeighbors
 {
